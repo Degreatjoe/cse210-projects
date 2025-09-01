2384b72dcd73709f65f205cae8fb5526596c2fef
Great <jgreat770@gmail.com>
2025-09-01 19:07:14 +0100

    exercise03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random numberGen = new Random();
+        int number = numberGen.Next(1, 100);
+        Console.WriteLine("Welcome to the guessing game.");
+        string inpoot;
+        int convertedInput;
+
+        do
+        {
+            Console.Write("Guess the number. ");
+            inpoot = Console.ReadLine();
+            convertedInput = int.Parse(inpoot);
+
+            if (convertedInput != number && convertedInput < number)
+        {
+            Console.WriteLine("Higher!");
+        }
+        else if (convertedInput != number && convertedInput > number)
+        {
+            Console.WriteLine("lower");
+        }
+        else
+        {
+                Console.WriteLine("You guessed it!");
+        }
+        } while (convertedInput != number);
+
     }
 }
