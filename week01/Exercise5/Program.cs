8c8c32db252ad95f67dcd1816e7fba7f42a01293
Great <jgreat770@gmail.com>
2025-09-02 10:34:02 +0100

    exercise05

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Transactions;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favourite number: ");
+        string userNumber = Console.ReadLine();
+        int convertednumber = int.Parse(userNumber);
+        return convertednumber;
+    }
+
+    static int SquareNumber(int args)
+    {
+        int squarednum = args * args;
+        return squarednum;
+    }
+
+    static void DisplayResult(string name, int squarednum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squarednum}");
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int numsquare = SquareNumber(num);
+        DisplayResult(name, numsquare);
     }
 }
