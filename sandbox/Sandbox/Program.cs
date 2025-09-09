f766e1a6e5d01f83b9db91a4395f60a3806cb8c6
Great <jgreat770@gmail.com>
2025-09-09 10:19:33 +0100

    week02-resume

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        // Console.WriteLine("Hello World! This is the Sandbox Project.");
+        int x;
+        int y;
+
+        try
+        {
+
+            Console.Write("enter a number: ");
+            string inp1 = Console.ReadLine();
+            x = int.Parse(inp1);
+
+            Console.Write("enter a seond number: ");
+            string inp2 = Console.ReadLine();
+            y = int.Parse(inp2);
+
+            int RES = x + y;
+            Console.WriteLine(RES);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e}");
+        }
+
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2001;
+        job1._endYear = 2005;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "aerosky";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Great Joseph";
+        myResume._myJobs.Add(job1);
+        myResume._myJobs.Add(job2);
+
+        myResume.Display();
     }
 }
