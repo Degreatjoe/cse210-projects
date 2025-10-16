12983b1702e7a3b7eca34362df6425f8e43189af
Great <jgreat770@gmail.com>
2025-10-16 10:15:49 +0100

    Exercise_tracking

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,18 @@
+// Derived class for Cycling
+public class Cycling : Activity
+{
+    private double _speedKph;
+
+    public Cycling(DateTime date, int minutes, double speedKph)
+        : base(date, minutes)
+    {
+        _speedKph = speedKph;
+    }
+
+    public override double GetSpeed() => _speedKph;
+
+    public override double GetDistance() => (_speedKph * Minutes) / 60;
+
+    public override double GetPace() => 60 / _speedKph;
+}
+
